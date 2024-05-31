646465404dd113323ab2ca62401bb18836b2a912
Юра Гуцалюк <hutsaluck@gmail.com>
2024-05-31 15:37:16 +0300

    Init condition

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <h2>Hello, {{name}}</h2>
+  <h2>Hello, {{ name }}</h2>
+  <br><br><br>
+  <button @click="flag = !flag">Click!</button>
+  <br>
+  <h2 v-if="flag">Hello</h2>
+  <h2 v-else>Bye bye</h2>
 </template>
 
 
 <script>
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
 
 export default defineComponent({
   name: 'App',
-  components: {
-
-  },
-  setup(){
+  components: {},
+  setup() {
     const name = 'Yuriy'
+    const flag = ref(true)
     return {
-      name
+      name,
+      flag
     }
   }
 })
