816c9609947568d61feecb86525eede3e9c8ddbf
Юра Гуцалюк <hutsaluck@gmail.com>
2024-05-31 15:31:19 +0300

    Create Vue Component

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>Hello, {{name}}</h2>
+</template>
+
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+
+  },
+  setup(){
+    const name = 'Yuriy'
+    return {
+      name
+    }
+  }
+})
+</script>
+<style>
+
+</style>
